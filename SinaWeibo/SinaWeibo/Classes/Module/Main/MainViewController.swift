6d89dbb2aaa1afc7f35f680df9d7b2e4bff17617
崔翔 <cuixiang@cuixiangdemacbook-pro.local>
2016-03-03 02:07:10 +0800

    2016.3.3

--- SinaWeibo/SinaWeibo/Classes/Module/Main/MainViewController.swift
+++ SinaWeibo/SinaWeibo/Classes/Module/Main/MainViewController.swift
@@ -2,34 +2,41 @@
 //  MainViewController.swift
 //  SinaWeibo
 //
-//  Created by 崔翔 on 16/2/28.
+//  Created by 崔翔 on 16/2/29.
 //  Copyright © 2016年 torresgeekhome. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //添加子视图控制器
+        addChildViewControllers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func addChildViewControllers(){
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        
     }
-    */
-
+    
+    private func addChildViewController(vc: UIViewController, title:String, imageName:String){
+        //设置颜色
+        self.tabBar.tintColor = UIColor.orangeColor()
+        //实例化导航视图控制器
+        let nav = UINavigationController(rootViewController: vc)
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        //        title = "首页"
+        //        nav.title = "首页"
+        //添加
+        addChildViewController(nav)
+    }
+    
 }
